1f9cdead6653bf9f11bac486c69551ca06bf8ecd
hunk12369 <kevinbryanmora3@gmail.com>
2024-01-25 19:17:58 -0400

    feat: categories

--- api/product.js
+++ api/product.js
@@ -1,9 +1,9 @@
 import axios from "./axios";
 
-export const getProduct = () => axios.get("/products");
+export const getProduct = () => axios.get("/client/products");
 export const getOneProduct = (productId) => {
   // Construir la URL de la solicitud GET con el ID del producto
-  const url = `/products/${productId}`;
+  const url = `/client/products/${productId}`;
 
   // Realizar la solicitud GET usando axios
   return axios.get(url);
