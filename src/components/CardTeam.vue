2440e3b9aee8902cf78cc881758008df9db1dc2c
micky <mickymolina@gmail.com>
2024-02-05 15:49:42 -0400

    New Page About Us

---
+++ src/components/CardTeam.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="grid grid-cols-2 w-max gap-10 pt-8">
+    <div v-for="person in personal" class="flex flex-col pt-4 w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <img class="place-self-center rounded-md w-72 h-auto" :src="person.imageSrc" alt="" />
+      <div class="flex flex-col p-5">
+        <h5 class="titlee mb-2 place-self-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{person.namePerson}}</h5>
+        <div class="flex gap-3 justify-center">
+          <v-icon name="io-mail" class="text-white"/>
+          <p class="titlee mb-3 font-normal text-gray-700 dark:text-gray-400">{{person.cellphone}}</p>
+        </div>
+        <div class="flex gap-3 justify-center">
+          <v-icon name="io-phone-portrait" class="text-white"/>
+          <p class="titlee mb-3 font-normal text-gray-700 dark:text-gray-400">{{person.email}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import foto1 from "../assets/curso1.jpg"
+import foto2 from "../assets/curso2.jpg"
+import foto3 from "../assets/curso3.jpg"
+import foto4 from "../assets/curso4.jpg"
+export default{
+  name: 'CardTeam',
+  data(){
+    return{
+      personal:[
+        {
+          imageSrc: foto1,
+          namePerson: 'Juan Juan Juan',
+          cellphone: '66666666',
+          email: 'abcdef@gmail.com'
+        },
+        {
+          imageSrc: foto1,
+          namePerson: 'nombre1',
+          cellphone: '66666666',
+          email: 'abcdef@gmail.com'
+        },
+        {
+          imageSrc: foto1,
+          namePerson: 'nombre1',
+          cellphone: '66666666',
+          email: 'abcdef@gmail.com'
+        },
+        {
+          imageSrc: foto1,
+          namePerson: 'nombre1',
+          cellphone: '66666666',
+          email: 'abcdef@gmail.com'
+        }]
+    }
+  }
+}
+</script>
+
+<style>
+.titlee {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ src/views/AboutUs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class=" flex-col pt-20">
+    <div class=" flex px-24 bg-[#001120] h-20 pt-3">
+      <h1 class="titlee text-4xl font-bold text-white">NUESTRO EQUIPO</h1>
+    </div>
+    <div class="flex justify-center">
+      <div class="flex flex-col w-3/4">
+        <div class="pl-36">
+          <CardTeam/>
+        </div>
+        <div class="bg-[#001120] h-20 pt-4">
+          <h1 class="titlee text-4xl font-bold text-white">NUESTRO EQUIPO</h1>
+        </div>
+      </div>
+      <div class="flex flex-col w-full bg-[#001120]">
+        <ImagePlace class="place-self-center"/>
+      </div>
+    </div>
+  </div>
+  <Footer/>
+</template>
+
+<script>
+  import CardTeam from "../components/CardTeam.vue";
+  import ImagePlace from "../components/ImagePlace.vue";
+  import Footer from "../components/Footer.vue"
+
+
+  export default{
+    components: {CardTeam, ImagePlace, Footer}
+  };
+
+</script>
+
+<style>
+.titlee {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
