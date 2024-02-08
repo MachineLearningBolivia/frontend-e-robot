f6e7d8700c4ce0fe69f46bf6ef9deb3f00c96a5a
Pabdro <pablis.badani@gmail.com>
2024-02-08 12:05:06 -0400

    arreglo axios

--- api/category.js
+++ api/category.js
@@ -1,6 +1,6 @@
 import axios from "./axios";
 
-export const getCategoriesRequest = () => axios.get("/client/categories");
+export const getCategoriesRequest = () => axios.get("/categories");
 
-export const getCategoryRequest = (id) => axios.get("/client/categories/" + id);
+export const getCategoryRequest = (id) => axios.get("/categories/" + id);
 
