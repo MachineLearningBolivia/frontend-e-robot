97cce9d0d507f4f676432aca42c5a7a8154a9014
micky <mickymolina@gmail.com>
2024-02-09 09:18:32 -0400

    Fixes

--- src/components/CardTeam.vue
+++ src/components/CardTeam.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-2 gap-10 pt-8">
-    <div v-for="person in personal" class="flex flex-col pt-4 w-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 md:w-80">
+    <div v-for="person in personal" class="flex flex-col pt-4 w-44 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 md:w-80">
       <img class="place-self-center rounded-md w-72 h-auto" :src="person.imageSrc" alt="" />
       <div class="flex flex-col p-5">
         <h5 class="titlee mb-2 text-center text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{person.namePerson}}</h5>
